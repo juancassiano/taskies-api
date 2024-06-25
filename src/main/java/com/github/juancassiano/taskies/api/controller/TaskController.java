9aad4969d735286a57b125e55cd7f0992304267e
Juan Lira <juancassiano@hotmail.com>
2024-06-24 21:47:06 -0300

    Refactor Create Method

--- src/main/java/com/github/juancassiano/taskies/api/controller/TaskController.java
+++ src/main/java/com/github/juancassiano/taskies/api/controller/TaskController.java
@@ -1,5 +1,45 @@
 package com.github.juancassiano.taskies.api.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.github.juancassiano.taskies.domain.entity.TaskEntity;
+import com.github.juancassiano.taskies.domain.service.TaskService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
-  
+
+  private final TaskService taskService;
+
+  public TaskController(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @GetMapping
+  public List<TaskEntity> getAllTasks() {
+    return taskService.findAllTasks();
+  }
+
+  @GetMapping("/{id}")
+  public TaskEntity getTaskById(@PathVariable Long id) {
+    return taskService.findTaskById(id);
+  }
+
+  @PostMapping
+  public void createTask(@RequestBody TaskEntity task) {
+    taskService.createTask(task);
+  }
+
+  @PutMapping("/{id}")
+  public TaskEntity updateTask(@PathVariable Long id, @RequestBody TaskEntity task) {
+    task.setId(id);
+    return taskService.updateTask(task);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteTask(@PathVariable Long id) {
+    taskService.deleteTask(id);
+  }
 }
