a9bab0f3cde75ff1565c2f2f670f64771f7beb33
Juan Lira <juancassiano@hotmail.com>
2024-06-25 00:08:52 -0300

    Create routes for API Controller

--- src/main/java/com/github/juancassiano/taskies/api/controller/CategoryController.java
+++ src/main/java/com/github/juancassiano/taskies/api/controller/CategoryController.java
@@ -2,10 +2,15 @@ package com.github.juancassiano.taskies.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.juancassiano.taskies.api.dto.CategoryDTO;
+import com.github.juancassiano.taskies.api.dto.input.CategoryInputDTO;
+import com.github.juancassiano.taskies.api.mapper.CategoryMapper;
 import com.github.juancassiano.taskies.domain.entity.CategoryEntity;
 import com.github.juancassiano.taskies.domain.service.CategoryService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -17,46 +22,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
-
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/categories")
 public class CategoryController {
 
   private CategoryService categoryService;
+  private CategoryMapper categoryMapper;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
         this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
     }
 
 
     @GetMapping()
-    public List<CategoryEntity> getAllCategory() {
-        return categoryService.findAllCategories();
+    public List<CategoryDTO> getAllCategory() {
+        List<CategoryEntity> tasks = categoryService.findAll();
+        return categoryMapper.toCategoryDTOList(tasks);
     }
 
     @GetMapping("/{id}")
-    public CategoryEntity findCategoryById(@PathVariable Long categoryId) {
-        CategoryEntity category = categoryService.findCategoryById(categoryId);
-        return category;
+    public CategoryDTO findCategoryById(@PathVariable Long id) {
+        CategoryEntity category = categoryService.findCategoryById(id);
+        return categoryMapper.toCategoryDTO(category);
     }
     
     @PostMapping()
-    public CategoryEntity createCategory(@RequestBody CategoryEntity entity) {
-        return categoryService.createCategory(entity);
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDTO createCategory(@RequestBody CategoryInputDTO categoryInputDTO) {
+        CategoryEntity category = categoryMapper.toDomainCategory(categoryInputDTO);
+        category = categoryService.createCategory(category);
+        return categoryMapper.toCategoryDTO(category);
     }
 
     @PutMapping("/{id}")
-    public CategoryEntity updateCategory(@PathVariable Long id, @RequestBody CategoryEntity categoryEntity) {
-        return categoryService.updateCategory(categoryEntity);
+    public CategoryDTO updateCategory(@PathVariable Long id, @RequestBody CategoryInputDTO categoryInputDTO) {
+        CategoryEntity categoryAtual = categoryService.findCategoryById(id);
+        categoryMapper.copyToDomainCategory(categoryInputDTO, categoryAtual);
+        categoryAtual = categoryService.updateCategory(categoryAtual);
+        return categoryMapper.toCategoryDTO(categoryAtual);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long categoryId) {
-      categoryService.deleteCategoryById(categoryId);  
-    }
-    
-    
-  
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
+      categoryService.deleteCategoryById(id);  
+    } 
 }
