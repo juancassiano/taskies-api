3bcaa7e4350b5c73f3b182f74678cddd6146837b
Juan Lira <juancassiano@hotmail.com>
2024-06-25 00:09:43 -0300

    Make Name Unique in BD

--- src/main/java/com/github/juancassiano/taskies/domain/entity/CategoryEntity.java
+++ src/main/java/com/github/juancassiano/taskies/domain/entity/CategoryEntity.java
@@ -2,6 +2,7 @@ package com.github.juancassiano.taskies.domain.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,19 +13,22 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "category")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @EqualsAndHashCode.Include
     private Long id;
     
     @NotBlank
+    @Column(unique = true)
     private String name;
     
     @OneToMany(mappedBy = "category")
