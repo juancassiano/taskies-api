930c4780f8a2cefb0643d996cc57b794a757cf82
Juan Lira <juancassiano@hotmail.com>
2024-06-25 00:09:07 -0300

    Create routes for API Controller

--- src/main/java/com/github/juancassiano/taskies/api/controller/TaskController.java
+++ src/main/java/com/github/juancassiano/taskies/api/controller/TaskController.java
@@ -2,8 +2,12 @@ package com.github.juancassiano.taskies.api.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.github.juancassiano.taskies.api.dto.TaskDTO;
+import com.github.juancassiano.taskies.api.dto.input.TaskInputDTO;
+import com.github.juancassiano.taskies.api.mapper.TaskMapper;
 import com.github.juancassiano.taskies.domain.entity.TaskEntity;
 import com.github.juancassiano.taskies.domain.service.TaskService;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -13,32 +17,44 @@ public class TaskController {
 
   private final TaskService taskService;
 
-  public TaskController(TaskService taskService) {
+  private TaskMapper taskMapper;
+
+  public TaskController(TaskService taskService, TaskMapper taskMapper) {
     this.taskService = taskService;
+    this.taskMapper = taskMapper;
   }
 
+  //TODO: Implementar filtro Done
   @GetMapping
-  public List<TaskEntity> getAllTasks() {
-    return taskService.findAllTasks();
+  public List<TaskDTO> getAllTasks() {
+    List<TaskEntity> tasks = taskService.findAllTasks();
+    return taskMapper.toTaskDTOList(tasks);
   }
 
   @GetMapping("/{id}")
-  public TaskEntity getTaskById(@PathVariable Long id) {
-    return taskService.findTaskById(id);
+  public TaskDTO getTaskById(@PathVariable Long id) {
+    TaskEntity taskEntity = taskService.findTaskById(id);
+    return taskMapper.toTaskDTO(taskEntity);
   }
 
   @PostMapping
-  public void createTask(@RequestBody TaskEntity task) {
-    taskService.createTask(task);
+  @ResponseStatus(HttpStatus.CREATED)
+  public TaskDTO createTask(@RequestBody TaskInputDTO taskInputDTO) {
+    TaskEntity taskEntity = taskMapper.toDomainTask(taskInputDTO);
+    taskEntity = taskService.createTask(taskEntity);
+    return taskMapper.toTaskDTO(taskEntity);
   }
 
   @PutMapping("/{id}")
-  public TaskEntity updateTask(@PathVariable Long id, @RequestBody TaskEntity task) {
-    task.setId(id);
-    return taskService.updateTask(task);
+  public TaskDTO updateTask(@PathVariable Long id, @RequestBody TaskInputDTO taskInputDTO) {
+    TaskEntity taskAtual = taskService.findTaskById(id);
+    taskMapper.copyToDomainTask(taskInputDTO, taskAtual);
+    taskAtual = taskService.updateTask(taskAtual);
+    return taskMapper.toTaskDTO(taskAtual);
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteTask(@PathVariable Long id) {
     taskService.deleteTask(id);
   }
