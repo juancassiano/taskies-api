0567664ea75a8295c78fee137adbffe5f7f3f524
Juan Lira <juancassiano@hotmail.com>
2024-06-27 22:09:47 -0300

    Adicioando função de security e oauth

---
+++ src/main/java/com/github/juancassiano/taskies/infra/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.github.juancassiano.taskies.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    return http
+      .authorizeHttpRequests(
+        authorizeConfig -> {
+          authorizeConfig.anyRequest().authenticated();
+        }
+      )
+      .oauth2Login(Customizer.withDefaults())
+      .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+      .build();
+  }
+}
